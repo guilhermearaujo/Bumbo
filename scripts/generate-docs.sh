27531ddbc0eb6bd5f1baecf33e837a2e14ff9db1
Guilherme Araújo <guilhermeama@gmail.com>
2018-08-16 23:20:23 -0300

    Add script to generate docs

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,9 @@
+jazzy \
+  --clean \
+  --author "Guilherme Araújo" \
+  --author_url "https://guilhermearaujo.me" \
+  --dash_url "https://guilhermearaujo.me/Bumbo/docsets/Bumbo.xml" \
+  --github_url "https://github.com/guilhermearaujo/Bumbo" \
+  --github_file_prefix "https://github.com/guilhermearaujo/Bumbo/tree/$(git describe --abbrev=0 --tags)" \
+  --module "Bumbo" \
+  --root_url "https://guilhermearaujo.me/Bumbo"
